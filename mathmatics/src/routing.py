62dc9dbb294d9cffe3686f93853e1002fd9050a2
togbonon <togbonon@togbonon.yeg>
2026-03-29 16:54:16 +0900

    update

--- mathmatics/src/routing.py
+++ mathmatics/src/routing.py
@@ -1,5 +1,5 @@
 from django.urls import re_path
-from consumers import CallConsumer
+from .consumers import CallConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/call/$', CallConsumer.as_asgi()),
