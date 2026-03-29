d427a8914fcfb0f8057f3187836b29cf74e6200c
togbonon <togbonon@togbonon.yeg>
2026-03-29 18:47:21 +0900

    update

--- mathmatics/mathmatics/settings.py
+++ mathmatics/mathmatics/settings.py
@@ -48,6 +48,10 @@ INSTALLED_APPS = [
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://mathmaticloves-production.up.railway.app",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
